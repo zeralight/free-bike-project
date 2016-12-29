d2df96b1774446ef2d7803512ede3da8b39fbc3e
Mélanie <mbochu@enseirb-matmeca.fr>
2016-12-29 16:30:14 +0000

    Relation.hpp edited online with Bitbucket

--- Relation.hpp
+++ Relation.hpp
@@ -1,17 +1,28 @@
 #ifndef RELATION_HPP
 #define RELATION_HPP
+#include <tulip/TlpTools.h>
+#include <tulip/Graph.h>
+#include <tulip/Edge.h>
+#include <tulip/Node.h>
 
+using namespace tlp;
 
 class Relation
 {
-  Relation();
-  ~Relation();
+private:
+  char * nameProperty[]; 
+  char * typeProperty[];
+  PropertyInterface* property[];
+  Graph * g;
+  int len;
   
-public:
-  int newRelationInstance();
-  int editRelationInsatance();
-  int deleteRelationInstance();
+public: 
+  Relation(Graph* G, std::string ,const char * [], const char * [], int );
+  ~Relation();
+  int newRelationInstance(const char *[] , const char *[] , const node, const node, int);
+  int deleteRelationInstance(const char *[] , const char *[] , int);
+  int editRelationInstance(const char *[] , const char *[] , int);
+  int getRelationInstance(const char * [], const char * [], int , struct edge*);
 };
-
 
 #endif
