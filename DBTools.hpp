37395680084f2459d7a30b4aaa2363990feab929
raptapia <raptapia@enseirb-matmeca.fr>
2017-01-26 12:42:46 +0100

    Created and documented the Database interface.

---
+++ DBTools.hpp
@@ -0,0 +1,36 @@
+#ifndef DBTOOLS_HPP
+#define DBTOOLS_HPP
+
+/* List of acceptable types for the attributes */
+#define DOUBLE "double"
+#define BOOLEAN "bool"
+#define INT "int"
+#define STRING "string"
+#define DATE "date"
+
+template <class T>
+struct AttrValue {
+  std::string label;
+  T value;
+
+  AttrValue (std::string label, T value);
+};
+
+struct AttrType {
+  std::string label;
+  std::string type;
+
+  AttrType (std::string label, std::string type);
+};
+
+template <class T>
+struct Attr {
+  std::string label;
+  std::string type;
+  T value;
+
+  Attr (std::string label, std::string type, T value);
+}
+  
+
+#endif DBTOOLS_HPP
