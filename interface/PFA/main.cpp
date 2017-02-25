7131c59b64d9eef53b055489c9aa640d99c9bb2c
raptapia <raptapia@enseirb-matmeca.fr>
2017-02-25 13:18:00 +0100

    Few corrections

--- interface/PFA/main.cpp
+++ interface/PFA/main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include "questionnairechaleur.h"
 #include <QApplication>
+#include <QTextCodec>
 #include "periodfilter.h"
+
 int main(int argc, char *argv[])
 {
-     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF_8"))
+  QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF_8"));
     QApplication a(argc, argv);
     MainWindow window;
     QWidget * mainWidget = new QWidget();
