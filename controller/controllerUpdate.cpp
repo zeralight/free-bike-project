3d3be1c6074e5c5ff329ccc6f0254c3832995718
leo <eole.33@hotmail.fr>
2017-03-18 16:32:33 +0100

    J'ai un poil avancé le controllerUpdate

--- controller/controllerUpdate.cpp
+++ controller/controllerUpdate.cpp
@@ -4,3 +4,12 @@
 
 #include "controllerUpdate.hpp"
 #include "../import/import.hpp"
+
+#include <wrappy/wrappy.h>
+
+ControllerUpdate::ControllerUpdate(){}
+ControllerUpdate::~ControllerUpdate(){}
+void ControllerUpdate::cityUpdate(enum Cities city){
+    wrappy::call(SCRIPT_FILES_DL+city+"/download_bike_"+city);
+    //Maintenant il faut appeler l'import
+}
--- controller/controllerUpdate.hpp
+++ controller/controllerUpdate.hpp
@@ -5,9 +5,23 @@
 #ifndef FREE_BIKE_PROJECT_REPOSITORY_CONTROLLERUPDATE_H
 #define FREE_BIKE_PROJECT_REPOSITORY_CONTROLLERUPDATE_H
 
+#ifndef SCRIPT_FILES_DL
+    #define SCRIPT_FILES_DL "../download_bike/"
+#endif
+#include <wrappy/wrappy.h>
 #include "controller.hpp"
 class ControllerUpdate{
     ControllerUpdate();
     ~ControllerUpdate();
+    void cityUpdate(enum Cities city);
+};
+
+enum Cities{
+    sanfrancisco = "sanfrancisco",
+    washington= "washington",
+    newyork = "newyork",
+    chicago = "chicago",
+    minneapolis = "minneapolis",
+    chattanooga = "chattanooga"
 };
 #endif //FREE_BIKE_PROJECT_REPOSITORY_CONTROLLERUPDATE_H
