c205a0fbdba74cdd2ed6361409ce8d99e47d0acd
raptapia <raptapia@enseirb-matmeca.fr>
2017-03-24 14:50:54 +0100

    Added some methods in Pattern

--- sgbd/Pattern.hpp
+++ sgbd/Pattern.hpp
@@ -2,6 +2,7 @@
 #define PATTERN_HPP
 
 #include <string>
+#include <unordered_map>
 
 #include <tulip/TlpTools.h>
 #include <tulip/Graph.h>
@@ -11,6 +12,8 @@
 #include <tulip/BooleanProperty.h>
 
 #include "DatabaseImpl.hpp"
+
+using namespace tlp;
 
 class Pattern {
 public:  
@@ -22,7 +25,8 @@ public:
 class PatternImpl : public Pattern {
   Graph * g;
   DatabaseImpl * db;
-  StringProperty * labelProp;
+  std::unordered_map<std::string, node> nodes;
+  std::unordered_map<std::string, edge> edges;
   StringProperty * typeProp;
   BooleanProperty * checkedProp;
 
@@ -33,14 +37,14 @@ public:
   void addNode(const std::string &label, const std::string &entityName);
   void addEdge(const std::string &label, const std::string &relationName, const std::string labelSrc, const std::string labelDst);
 
-  std::string getType(tlp::node);
-  std::string getType(tlp::edge);
+  std::string getEntity(node n);
+  std::string getRelation(edge e);
 
-  tlp::node getNode(std::string label);
-  tlp::edge getEdge(std::string label);
+  node getNode(std::string label);
+  edge getEdge(std::string label);
 
-  std::string isChecked(tlp::node);
-  std::string isChecked(tlp::edge);
+  std::string isChecked(node n);
+  std::string isChecked(edge e);
 
   bool isNode(std::string label);
   bool isEdge(std::string label);
